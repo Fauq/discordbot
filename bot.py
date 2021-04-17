7435a6b4d807ecbe931dd44a77fc066f8568fb50
Fauq <68355398+Fauq@users.noreply.github.com>
2021-04-16 20:19:43 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands 
+import datetime
+import asyncio
+import random
+import time
+client = commands.Bot(command_prefix = '*')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.event
+async def on_guild_channel_create(channel):
+    channel = client.get_channel(channel.id)
+    embed = discord.Embed(title = '<:uc_gift2:832737214224007188> WELCOME TO YOUR INVITE REWARDS TICKET <:uc_gift2:832737214224007188>', description = 'Please follow the format below so we can ensure you get your reward fast and smooth\n\n ```Amount of invites:\nUB or ROBUX (30% less if robux):\nGamepass/Shirt link (if you chose Robux):\nScreenshot of how you got your invites:```', colour = 0xa823fb)
+    embed_2 = discord.Embed(title = '<:uc_gift2:832737214224007188> WELCOME TO YOUR LEVEL REWARDS TICKET <:uc_gift2:832737214224007188>', description = 'Please follow the format below so we can ensure you get your reward fast and smooth\n\n ```Your level:\nProof of level (type !rank in chat)```', colour = 0xa823fb)
+    embed_3 = discord.Embed(title = '<:uc_gift2:832737214224007188> WELCOME TO YOUR INVITE EVENT TICKET <:uc_gift2:832737214224007188>', description = 'Please follow the format below so we can ensure you get your reward fast and smooth\n\n ```Amount of invites:\nGamepass/Shirt link (if you are claiming robux):\nScreenshot of how you got your invites:```', colour = 0xa823fb)
+    await asyncio.sleep(2)  
+    if channel.name.startswith('invite-rewards'):
+        await channel.send(embed=embed)
+    elif channel.name.startswith('level-rewards'):
+        await channel.send(embed=embed_2)
+    elif channel.name.startswith('invite-event'):
+        await channel.send(embed=embed_3)   
+    return 
+
+
+
+
