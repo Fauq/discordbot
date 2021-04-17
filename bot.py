81d7e33dc6c1c37a2dc4d7df31f306731686b879
Haotian <lihao9440@gmail.com>
2021-04-17 17:51:07 -0400

    e

--- bot.py
+++ bot.py
@@ -25,9 +25,10 @@ async def on_guild_channel_create(channel):
         await channel.send(embed=embed_3)   
     return 
 
-@client.command()
+@client.command(pass_context=True)
 async def won(ctx, arg1, arg2):
     await ctx.channel.send("╭ <:uc_gift1:832730994406719489> [ <@{}> ] won the `{}` drop! Go ask them if we are legit!\n<:uc_dot:832731636063535124> <:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684> <:uc_dot:832731636063535124>\n<:uc_info:832732093238607939> **TIPs:**\n<:uc_dots832735257152192522> Put our server above others to easily see pings!\n<:uc_dots832735257152192522> Put `discord.gg/bobux` in your status for extra claim time!\n<:uc_dots832735257152192522> Join all our drops to win `Nitro/Robux` easily!\n<:uc_dot:832731636063535124> <:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684><:uc_line:833093465943834684> <:uc_dot:832731636063535124>\n╰ <a:vibecat:821107739245150218> **Stay active for more drops!**".format(arg1, arg2))
+    await client.delete_message(ctx.message)
 
 @client.command()
 async def claimed(ctx, arg1, arg2, arg3):
