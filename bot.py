76bbf3966395715d4a3c1946f31d18c822c36ced
Haotian <lihao9440@gmail.com>
2021-04-17 17:10:36 -0400

    e

--- bot.py
+++ bot.py
@@ -27,7 +27,11 @@ async def on_guild_channel_create(channel):
 
 @client.command()
 async def won(ctx, arg1, arg2):
-    await ctx.channel.send("╭ <:uc_gift1:832730994406719489> <@{}> won the {} drop! Go ask them if we are legit!".format(arg1, arg2))
+    await ctx.channel.send("╭ <:uc_gift1:832730994406719489> <@[ {} ]> won the **{}** drop! Go ask them if we are legit!".format(arg1, arg2))
 
+@client.command(pass_context=True)
+async def claimed(ctx, arg1, arg2):
+    channel = client.get_channel(689638287429992469)
+    await ctx.channel.send("╭ <:uc_gift1:832730994406719489> <@[ {} ]> won the **{}** drop! Go ask them if we are legit!".format(arg1, arg2))
 
 client.run("ODAyMzU0Mzg1MjE2MzM5OTk4.YAuAwA.Lywh6zXvGkW74EkcWjpTl-PCGLo")
