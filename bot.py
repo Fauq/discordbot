49c80e305ad38674f61b0cb39841086acc5b710f
Anonymous-Troll <68359204+Anonymous-Troll@users.noreply.github.com>
2021-04-23 09:37:17 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     print('Bot is ready.')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"over {len(client.users)} users! .gg/bobux"))
+   
 
 @client.event
 async def on_guild_channel_create(channel):
