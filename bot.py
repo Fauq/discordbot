6bb4591556aebdb6a0218448cfeb19232bcb2623
Haotian <lihao9440@gmail.com>
2021-04-26 10:15:44 -0400

    e

--- bot.py
+++ bot.py
@@ -158,7 +158,7 @@ async def won(ctx, arg1, arg2):
 
 @client.command()
 @has_permissions(administrator=True)
-async def claimed(ctx, arg1, arg2, arg3):
+async def claimed(ctx, arg1, arg2, *, arg3):
     channel = client.get_channel(804402670940454973)
     await channel.send("[ <@{}> ] claimed `{}` from {}".format(arg1, arg2, arg3))
     await ctx.message.delete()
