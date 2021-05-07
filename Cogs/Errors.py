c706196c6afdd7aa1a1d99b9f46672bc122c9ca9
Anonymous-Troll <68359204+Anonymous-Troll@users.noreply.github.com>
2021-05-07 11:14:02 -0700

    Add files via upload

---
+++ Cogs/Errors.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+
+
+class Errors(commands.Cog, name="Error"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+
+        embed=discord.Embed(title="Error with executed command: ", description=f"`{error}`")
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Errors(bot))
+    print("Error handler is ready!")
