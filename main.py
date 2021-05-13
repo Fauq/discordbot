d499c22bc5bd23f22ef879af926cd212d8a8684f
Anonymous-Troll <68359204+Anonymous-Troll@users.noreply.github.com>
2021-05-13 16:07:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ class MyNewHelp(commands.MinimalHelpCommand):
     async def send_pages(self):
         destination = self.get_destination()
         for page in self.paginator.pages:
-            emby = discord.Embed(description=page)
-            await destination.send(embed=emby, color=discord.Colour.blue())
+            emby = discord.Embed(description=page, color=discord.Colour.blue())
+            await destination.send(embed=emby)
 
 client.help_command = MyNewHelp()
 
