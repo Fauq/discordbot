14485f8177f584621f1d9786d1502cc42020bf2e
Anonymous-Troll <68359204+Anonymous-Troll@users.noreply.github.com>
2021-05-13 17:36:08 -0700

    Update Moderation.py

--- Cogs/Moderation.py
+++ Cogs/Moderation.py
@@ -58,7 +58,10 @@ class Moderation(commands.Cog, name="Mod"):
         async with aiohttp.ClientSession() as session:
             channelID = 786735734626713600
             channel = ctx.guild.get_channel(channelID)
-            reason = "No Reason Provided"
+            
+            if reason == None:
+                reason = "No reason provided"  
+                
             webhook = Webhook.from_url('https://discord.com/api/webhooks/839170018142847008/h2bdEgBozIheGZehzYBlMb_tMwHWg3dbI-bT7cnC441XO8iUDNhKNFaHLMDGywAd3PvF', adapter=AsyncWebhookAdapter(session))
             embed = discord.Embed(description=f"***{user} was banned*** | {reason}", color=discord.Color.blue())
             user_embed = discord.Embed(title="Banned!", description=f"You were banned in **{user.guild}** for **{reason}**.", color=discord.Color.blue())
@@ -112,7 +115,10 @@ class Moderation(commands.Cog, name="Mod"):
 
         channelID = 786735734626713600
         channel = ctx.guild.get_channel(channelID)
-        reason = "No Reason Provided"
+       
+        
+        if reason == None:
+            reason = "No reason provided"
 
         embed=discord.Embed(title="Kicked", description=f"*{member} was kicked* | {reason}", color=discord.Color.red())
         user_embed = discord.Embed(title="Kicked!", description=f"You were kick from **{member.guild}** for **{reason}**.", color=discord.Color.blue())
