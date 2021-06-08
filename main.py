42763be432d3b67e932a7641e084b550b2ef9703
Anonymous-Troll <68359204+Anonymous-Troll@users.noreply.github.com>
2021-06-08 11:09:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ client.load_extension("jishaku")
 @client.event
 async def on_ready():
     print('Bot is ready.')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=".gg/bobux in status for free robux"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=".gg/universal"))
    
 
 initial_extensions = [
